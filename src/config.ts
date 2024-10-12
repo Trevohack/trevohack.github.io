46f3aed3c45b4313e8a967d368b03b8e706975c1
!0xTrev <136177431+Trevohack@users.noreply.github.com>
2024-10-12 13:38:35 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ export const siteConfig: SiteConfig = {
   },
   banner: {
     enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/site-banner.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: false,         // Display the credit text of the banner image
@@ -47,9 +47,9 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/logo_site.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Trevohack',
+  bio: 'Security Enthusiast | Rootkit Researcher | Bug Hunter | Student',
   links: [
     {
       name: 'GitHub',
